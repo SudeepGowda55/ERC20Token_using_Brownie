9a50f1ba4dca676a6a637165dddec7ba3ac5ce3a
SudeepGowda55 <ssudeepgowda55@gmail.com>
2022-10-11 00:47:40 +0530

    first commit

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,4 +2,4 @@ from brownie import accounts, config, MyToken
 
 def main():
    account=accounts.add(config["wallets"]["from_key"])
-   erc20=MyToken.deploy({"from":account})
+   erc20=MyToken.deploy({"from":account}, publish_source=True)
